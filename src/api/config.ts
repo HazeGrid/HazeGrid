fd5656bb37c41983c9dab1109508964e6b5159ba
Dusty <akshatmehrotra@outlook.de>
2025-04-23 00:42:47 +0530

    Delete src directory

--- src/api/config.ts
+++
@@ -1,9 +0,0 @@
-export const API_CONFIG = {
-  BASE_URL: "https://api.tomorrow.io/v4/weather",
-  GEO: "https://api.tomorrow.io/v4/locations",
-  API_KEY: import.meta.env.VITE_TOMORROW_API_KEY,
-  DEFAULT_PARAMS: {
-    units: "metric",
-    apikey: import.meta.env.VITE_TOMORROW_API_KEY,
-  },
-};
