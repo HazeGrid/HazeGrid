a6054ac2fdd888755297834ab249cd56e168018c
Dusty <akshatmehrotra@outlook.de>
2025-04-22 19:47:33 +0530

    Delete app directory

--- app/route.tsx
+++
@@ -1,7 +0,0 @@
-import { getLocations } from "@/lib/api";
-
-export async function GET(request: Request) {
-  const query = new URL(request.url).searchParams.get("query");
-  if (!query) return Response.error();
-  return Response.json(await getLocations({ query }));
-}
