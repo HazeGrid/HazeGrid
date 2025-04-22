110cd6334c2577ec4952f158acd2c54efab224b9
Dusty <akshatmehrotra@outlook.de>
2025-04-22 17:05:18 +0530

    Update weather.js

--- api/weather.js
+++ api/weather.js
@@ -1,20 +1,17 @@
 export default async function handler(req, res) {
   const apiKey = process.env.WEATHER_API_KEY;
-
   const { location, lat, lon } = req.query;
-
   const query = location
     ? `location=${location}`
     : lat && lon
     ? `location=${lat},${lon}`
-    : `location=Jaipur`; // fallback
+    : `location=Jaipur`;
 
   const url = `https://api.tomorrow.io/v4/weather/forecast?${query}&timesteps=1h,1d&apikey=${apiKey}`;
 
   try {
     const response = await fetch(url);
     const data = await response.json();
-
     const current = data.timelines.daily[0].values;
 
     res.status(200).json({
