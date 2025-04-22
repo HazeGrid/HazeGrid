1a9c0abe8d39c97cb55f6f485f9286e0f7548ccd
Dusty <akshatmehrotra@outlook.de>
2025-04-22 19:16:10 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,8 @@
 import './globals.css';
-import { Inter } from 'next/font/google';
-
-const inter = Inter({ subsets: ['latin'] });
-
-export const metadata = {
-  title: 'HazeGrid',
-  description: 'Weather forecast by HazeGrid',
-};
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
